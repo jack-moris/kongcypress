75120eeca37b2f9cb68a8acda551d73a36743098
jackmoris <jackzhao39@gmail.com>
2024-11-11 14:32:34 +0700

    update cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,17 @@
 describe('kong cp smoke test', () => {
   beforeEach(() => {
-    //visit home page
+    //visit kong cp home page
     cy.visit('http://localhost:8002/workspaces')
   })
              
   it('click default workspace ', () => {
+    //find the default workspace link, and click.
     cy.get('.workspace-name').should('have.text', 'default')
     cy.get('.workspace-name').click()
+
+    //check the default page is loaded.
+    cy.get('.title').should('have.text','Overview')
+    cy.url().should('include', '/default/overview')
   })
+
 })
