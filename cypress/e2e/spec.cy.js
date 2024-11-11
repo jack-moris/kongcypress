1b6aaa21a37577048fee5e37da93b01ca6d656cd
jack-moris <jackzhao39@gmail.com>
2024-11-11 13:53:22 +0700

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('kong cp smoke test', () => {
+  beforeEach(() => {
+    //visit home page
     cy.visit('http://localhost:8002/workspaces')
+  }
+             
+  it('click default workspace ', () => {
+    cy.get('.workspace-name').should('have.text', 'default')
+    cy.get('.workspace-name').click()
   })
 })
