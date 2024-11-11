7cd94a3f5913884a1aedc0b81f1f62c9c68ac477
jackmoris <jackzhao39@gmail.com>
2024-11-11 20:05:48 +0700

    add some comment for curl task

--- cypress.config.js
+++ cypress.config.js
@@ -10,22 +10,43 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on("task",{
         
+        //It is to check if RoutePath works or not in kong gateway using simple curl.
         execCurl(command){
           return new Promise((resolve, reject) => {
             exec(command, (error, stdout, stderr) => {
-
-
               if (error) {
                 console.error(`exec error: ${error}`);
                 return reject({ error: error.message, stderr });
-              
               }
-              console.log(stdout);
+              //console.log(stdout);
+              // for example, curl -x GET http://localhost:8000/Tec0dyRg
+              // in wich Tec0dyRg is a route path. it might reponse like this:
+              // we use https://postman-echo.com/get as a target service.
+              // {
+              //   "args": {},
+              //   "headers": {
+              //     "host": "postman-echo.com",
+              //     "x-request-start": "t=1731330029.357",
+              //     "connection": "close",
+              //     "x-forwarded-proto": "https",
+              //     "x-forwarded-port": "443",
+              //     "x-amzn-trace-id": "Root=1-6731ffed-0274d7c2560843c54a1506b6",
+              //     "via": "1.1 kong/3.8.0.0-enterprise-edition",
+              //     "x-forwarded-host": "localhost",
+              //     "x-forwarded-path": "/sHnGrgls",
+              //     "x-forwarded-prefix": "/sHnGrgls",
+              //     "x-kong-request-id": "eec7840ddb652e316ac0a029be7c1b17",
+              //     "user-agent": "curl/7.81.0",
+              //     "accept": "*/*"
+              //   },
+              //   "url": "https://postman-echo.com/get"
+              // }
               return resolve(stdout);
             });
         })
       },
 
+        //It is to fetch data from db, the purpose is to check the data is persistent or not.
         async readfromDB(query) {
           const client = new Client({
             user: "kong",
