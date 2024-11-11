8a6c549ec0dc1366dbe0b96e9e8bc55041b37c25
jack-moris <jackzhao39@gmail.com>
2024-11-12 04:22:54 +0700

    Update smoke.cy.js

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -1,12 +1,16 @@
 describe('kong cp smoke test', () => {
   beforeEach(() => {
-    //visit kong cp home page
-    cy.visit('http://localhost:8002/workspaces')
+
+    //TODO: do some db cleaning job.
+    //It is better to use API CALL to clean the job rather than DB operation.
+
   })
   
   it('CASE1-Check Homepage Loadable: \n\
     \tStep1: Click default workspace from kong cp homepage \n\
     \tStep2: Check the workspace page is good\n', () => {
+    
+    cy.visit('http://localhost:8002/workspaces')
     //find the default workspace link, and click.
     cy.get('div[class="workspace-name"]').should('have.text', 'default')
     cy.get('div[data-testid="workspace-link-default"]').click()
@@ -65,6 +69,7 @@ describe('kong cp smoke test', () => {
     const serviceTags= generateRandomString()+','+generateRandomString()+","+generateRandomString()
     const serviceUrl = "https://postman-echo.com/get" // this is a site for testing route.
 
+    cy.visit('http://localhost:8002/workspaces')
     //STEP1: Create a service and its route.
     //find the default workspace link, and click.
     cy.get('div[data-testid="workspace-link-default"]').click()
@@ -183,11 +188,12 @@ describe('kong cp smoke test', () => {
 
 
   it('CASE4-Check Route Method Restriction: \n\
-    \tStep1: Create a service and its route, the route limits Only Post method \n\
+    \tStep1: Create a service and its route, the route allows Only Post method \n\
     \tStep2: Check Get method does not work for the route\n\
     \tStep3: Modify the method limit, enable Get method\n\
     \tStep4: Check Get method works good now\n\
     \tStep5: Remove route and service\n', () => {
+
       const generateRandomString = (length = 8) => {
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let result = '';
@@ -196,9 +202,6 @@ describe('kong cp smoke test', () => {
         }
         return result;
       };
-  
-      //Visit gateway service page directly.
-      cy.visit('http://localhost:8002/default/services')
 
       // Generate a random string for name,tags and url for the service form submit.
       const serviceName = generateRandomString()
@@ -206,8 +209,8 @@ describe('kong cp smoke test', () => {
       const serviceUrl = "https://postman-echo.com/get" // this is a site for testing route.
   
       //STEP1: Create a service and its route.
-      //actually go to page: http://localhost:8002/default/services/create
-      cy.get('a[data-testid="new-gateway-service"]').click()
+      //Create a service
+      cy.visit('http://localhost:8002/default/services/create')
       cy.get('input[data-testid="gateway-service-name-input"]').type(serviceName)
       cy.get('input[data-testid="gateway-service-tags-input"]').type(serviceTags)
       cy.get('input[data-testid="gateway-service-url-input"]').type(serviceUrl)
@@ -235,13 +238,12 @@ describe('kong cp smoke test', () => {
       cy.get('div[data-testid="name-plain-text"]').should('contains.text',routeName)
       
       //STEP2: Check Get method does not work for the route
-      //Important! first of all, need to wait 5s for route taking effect.
+      //Important! first of all, need to wait 5s for route taking effect
       cy.wait(5000) 
-      cy.task('execCurl', 'curl -X GET http://localhost:8000'+routePaths)
-      .then((stdout) => {
+      cy.task('execCurl', 'curl -X GET http://localhost:8000'+routePaths).then((stdout) => {
         expect(stdout).to.contain('no Route');//should not work for Get method
       });
-            
+    
       //STEP3: Edit Route to enable GET method.
       //click Route actions button to find Edit choice drop down.
       cy.get('button[data-testid="header-actions"]').click()
@@ -249,15 +251,98 @@ describe('kong cp smoke test', () => {
       //Allow GET Method
       cy.get('input[data-testid="get-method-toggle"]').check({force:true})
       cy.get('button[data-testid="route-form-submit"]').click()
-      
+    
       //STEP4: Check Get method works good now
       //Important! first of all, need to wait 5s for route taking effect.
       cy.wait(5000) 
-      cy.task('execCurl', 'curl -X GET http://localhost:8000'+routePaths)
+      cy.task('execCurl', 'curl -X GET http://localhost:8000'+routePaths).then((stdout) => {
+        expect(stdout).to.contain('postman');//should work for Get method
+      });
+
+      //STEP5: Remove the route.
+      //now check to remove the route.
+      cy.get('button[data-testid="header-actions"]').click()
+      cy.get('span').contains('Delete').click()
+      cy.get('input[data-testid="confirmation-input"]').type(routeName)
+      cy.get('button[data-testid="modal-action-button"]').click()
+  
+      //STEP6: Remove the service.
+      //Now check to remove service
+      //click the drag down list.
+      cy.get('button[data-testid="header-actions"]').click()
+      cy.get('span').contains('Delete').click()
+      cy.get('input[data-testid="confirmation-input"]').type(serviceName)
+      cy.get('button[data-testid="modal-action-button"]').click()
+  
+      //Ends here. seems all good.
+  })
+
+  it('CASE5-Check Multiple Paths for one Route : \n\
+    \tStep1: Create a service, and creates 50 paths for one route \n\
+    \tStep2: Check any path should work good\n\
+    \tStep3: Remove route and service\n', () => {
+
+      const generateRandomString = (length = 8) => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+          result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+      };
+
+      // Generate a random string for name,tags and url for the service form submit.
+      const serviceName = generateRandomString()
+      const serviceTags= generateRandomString()+','+generateRandomString()+","+generateRandomString()
+      const serviceUrl = "https://postman-echo.com/get" // this is a site for testing route.
+  
+      //STEP1: Create a service and its route with multple paths
+      cy.visit('http://localhost:8002/default/services/create')
+      cy.get('input[data-testid="gateway-service-name-input"]').type(serviceName)
+      cy.get('input[data-testid="gateway-service-tags-input"]').type(serviceTags)
+      cy.get('input[data-testid="gateway-service-url-input"]').type(serviceUrl)
+      cy.get('button[data-testid="service-form-submit"]').click()
+
+      // Create a route for this service.
+      //click service record just created
+      cy.get('tr[data-testid=\"'+serviceName+'\"]').click()
+
+      //click to create paths repeatedly for 5 times.
+      cy.get('button').contains('Add a Route').click()
+      const routeName = generateRandomString()
+      const routeTags = generateRandomString()+','+generateRandomString()+','+generateRandomString()
+      const routePaths = [];
+      routePaths[0]= '/'+generateRandomString()
+      
+      cy.get('input[data-testid="route-form-name"]').type(routeName)
+      cy.get('input[data-testid="route-form-tags"').type(routeTags)
+
+      //repeatedly click to add new Paths
+      //Initial operation, input the first path.
+      routePaths[0]= '/'+generateRandomString();
+      cy.get('input[data-testid="route-form-paths-input-1').type(routePaths[0])
+      for(let i=1;i<50;i++){//4 times to input more paths
+        cy.get('button[data-testid="add-paths"]:not([disabled])').click()
+        routePaths[i]= '/'+generateRandomString();
+        cy.get('input[data-testid="route-form-paths-input-'+(i+1)).type(routePaths[i])
+        
+      }  
+      //click the form to submit the route creation
+      cy.get('button[data-testid="route-form-submit"]').click()
+      //check route should be created successfully. There created a record of Route.
+      cy.get('span').contains(routeName).click()  
+      //check route should be created successfully with the exact routeName input.
+      cy.get('div[data-testid="name-plain-text"]').should('contains.text',routeName)
+      
+      //STEP2: Check Get method should work for any path of the route
+      //Important! first of all, need to wait 5s for route taking effect.
+      cy.wait(5000) 
+      cy.task('execCurl', 'curl -X GET http://localhost:8000'+routePaths[1])//random choose one path to check
       .then((stdout) => {
         expect(stdout).to.contain('postman');//should work for Get method
       });
-             
+            
+               
       //STEP5: Remove the route.
       //now check to remove the route.
       cy.get('button[data-testid="header-actions"]').click()
@@ -276,5 +361,37 @@ describe('kong cp smoke test', () => {
       //Ends here. seems all good.
 
   })
+
+
+  it('CASE6-[TODO] Check Same Route Name Conflicts on Creation : \n\
+    \tStep1: Create a service, and creates 2 routes with same name \n\
+    \tStep2: Check the second operation SHOULD FAILE because route name is unique when it is not empty string\n\
+    \tStep3: Remove route and service\n', () => {
+      //TODO
+  })
+ 
+  it('CASE7-[TODO]Check Create 999 Routes Pagination : \n\
+    \tStep1: Create a service, and creates 999 routes with different names \n\
+    \tStep2: Check pagination of Routes works good\n\
+    \tStep3: Remove routes and service\n', () => {
+      //TODO
+  })
+
+  it('CASE8-[TODO]]Check 999 services creation and pagination: \n\
+    \tStep1: Create 999 service with different names \n\
+    \tStep2: Check pagination of Services works good\n\
+    \tStep3: Remove services\n', () => {
+      //TODO
+  })  
+
+
+  it('CASE9-[TODO]Check Service Deletion Must Fail With Existing Route(s) : \n\
+    \tStep1: Create a service, and creates one route \n\
+    \tStep2: Check if user is able to delete this service directly, should failed, and have tips\n\
+    \tStep3: Remove route and service\n', () => {
+      //TODO
+  })  
+
+  //TO Add more cases here:
 
 })
