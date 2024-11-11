8f07c9bbc8036639f1b1dfd41e9e9de84ae95ba5
jack-moris <jackzhao39@gmail.com>
2024-11-11 09:38:55 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "ijucgi",
+  video:true,
+  videoCompression: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
